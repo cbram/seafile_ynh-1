7ce05182daee9dea1960a5e2972778b5d38dcba6
Andrea Cotza <andrea.cotza@icloud.com>
2015-03-22 05:23:33 +0100

    Modified update system

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ "$seafile_version" == "$current_version_full" ]; then
+  echo "Info: No updates are available at the moment, if a new version of seafile is out we will port the update soon."
+fi
