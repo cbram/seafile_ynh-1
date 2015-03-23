033833bbabb1c63d1f0b6c99bc86b3160aaec16b
Andrea Cotza <andrea@cotza.me>
2015-03-23 18:56:29 +0100

    Use mysql and configure email server
    A big thanks to @mbugeia

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ "$seafile_version" == "$current_version_full" ]; then
-  echo "Info: No updates are available at the moment, if a new version of seafile is out we will port the update soon."
-fi
